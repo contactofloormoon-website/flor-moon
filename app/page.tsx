34bf49af5291ac6b61b49a69885a0f1dfd1f2b06
urbanized <rosariogrooverecords@gmail.com>
2026-04-01 21:28:40 -0300

    configuracion para cloudflare2

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,10 @@
 export const runtime = 'edge';
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Web de Flor Moon</h1>
+      {/* El resto de tu código aquí */}
+    </div>
+  );
+}
