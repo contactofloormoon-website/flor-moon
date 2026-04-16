3391e055466521d398870e3b5b38a37d2bcaecf8
urbanized <rosariogrooverecords@gmail.com>
2026-04-16 18:43:15 -0300

    maqueta numero22

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,8 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import cloudflare from '@astrojs/cloudflare';
 
-// https://astro.build/config
 export default defineConfig({
-  output: 'server', // Mantiene el sitio en modo servidor
-  adapter: cloudflare({
-    mode: 'directory', // ESTA LÍNEA es la que arregla los errores 404 en subpáginas
-    platformProxy: {
-      enabled: true,
-    },
-  }),
+  output: 'hybrid',
+  adapter: cloudflare(),
   integrations: [react()],
 });
