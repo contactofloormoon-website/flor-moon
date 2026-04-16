b05e57e9a0b734724363150c6ff2827f802c866a
urbanized <rosariogrooverecords@gmail.com>
2026-04-16 19:11:37 -0300

    maqueta numero31

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import cloudflare from '@astrojs/cloudflare';
 
+// https://astro.build/config
 export default defineConfig({
-  // En Astro 6, 'static' es el valor correcto para sitios con algunas funciones de servidor
-  output: 'static', 
-  adapter: cloudflare(),
   integrations: [react()],
 });
