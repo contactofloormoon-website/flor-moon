14f9f504700330ed8493565043ac5dd7f7de483f
urbanized <rosariogrooverecords@gmail.com>
2026-04-16 21:24:32 -0300

    probando todo nuevo1

--- src/pages/contact.ts
+++
@@ -1,8 +0,0 @@
-export const prerender = true;
-
-export async function POST({ request }: { request: Request }) {
-  return new Response(JSON.stringify({ 
-    success: false, 
-    error: 'Formulario deshabilitado temporalmente' 
-  }), { status:503 });
-}
