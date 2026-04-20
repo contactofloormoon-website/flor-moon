8cd8129091655152ca9339afb62b4f957987ce6b
urbanized <rosariogrooverecords@gmail.com>
2026-04-20 19:13:40 -0300

    probando todo nuevo15

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  site: 'https://flor-moon.pages.dev',
   integrations: [react()],
 });
